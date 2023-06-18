4c45f44acd5e331e773a2577618d42e14e5f18ad
annrobin26 <annmaryrobin26@gmail.com>
2023-06-18 18:03:28 -0400

    initial commit

---
+++ inventory-service/src/main/java/com/ecommerce/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,26 @@
+package com.ecommerce.inventoryservice.controller;
+import com.ecommerce.inventoryservice.dto.InventoryResponse;
+import com.ecommerce.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+        log.info("Received inventory check request for skuCode: {}", skuCode);
+        return inventoryService.isInStock(skuCode);
+    }
+}
